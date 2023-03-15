5bf59421b78781fc5c110c9d0ae39d55ea2bb318
Nicole Garcia <nicolsss>
2023-03-15 12:59:07 -0500

    add: fixtures for beverage test

---
+++ app/test/fixtures/beverage.py
@@ -0,0 +1,15 @@
+import pytest
+
+from ..utils.functions import get_random_price, get_random_string
+
+
+def beverage_mock() -> dict:
+    return {
+        'name': get_random_string(),
+        'price': get_random_price(10, 20)
+    }
+
+
+@pytest.fixture
+def beverage():
+    return beverage_mock()
