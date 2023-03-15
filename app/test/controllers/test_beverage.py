fcb701b6ae5e6b3b7e6a5b92d51291358366a439
Nicole Garcia <nicolsss>
2023-03-15 12:39:46 -0500

    test: initial beverage controller

---
+++ app/test/controllers/test_beverage.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+def test_create(app, beverage: dict):
+    return
+
+
+def test_update(app, beverage: dict):
+    return
+
+
+def test_get_by_id(app, beverage: dict):
+    return
+
+
+def test_get_all(app, beverages: list):
+    return
