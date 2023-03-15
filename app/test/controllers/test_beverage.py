38231e5daea00a5511abb0c18a37d258115187c7
Nicole Garcia <nicolsss>
2023-03-15 13:10:08 -0500

    test: beverage test_create

--- app/test/controllers/test_beverage.py
+++ app/test/controllers/test_beverage.py
@@ -1,8 +1,14 @@
 import pytest
+from app.controllers import BeverageController
 
 
 def test_create(app, beverage: dict):
-    return
+    created_beverage, error = BeverageController.create(beverage)
+    pytest.assume(error is None)
+    for param, value in beverage.items():
+        pytest.assume(param in created_beverage)
+        pytest.assume(value == created_beverage[param])
+        pytest.assume(created_beverage['_id'])
 
 
 def test_update(app, beverage: dict):
