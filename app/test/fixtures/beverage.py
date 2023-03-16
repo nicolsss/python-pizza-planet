1f31ada0248c40f198a168198ed6e179f121b124
nicolsss <56645701+nicolsss@users.noreply.github.com>
2023-03-15 19:48:35 -0500

    feat: PIZ2.3 Add beverage service (#5)
    * test: initial create beverage service
    
    * add: create beverage service
    
    * test: initial update beverage service
    
    * add: update beverage service
    
    * add: get by id beverage service
    
    * add: get all beverage service
    
    ---------
    
    Co-authored-by: Nicole Garcia <nicolsss>

--- app/test/fixtures/beverage.py
+++ app/test/fixtures/beverage.py
@@ -11,6 +11,11 @@ def beverage_mock() -> dict:
 
 
 @pytest.fixture
+def beverage_uri():
+    return '/beverage/'
+
+
+@pytest.fixture
 def beverage():
     return beverage_mock()
 
@@ -18,3 +23,18 @@ def beverage():
 @pytest.fixture
 def beverages():
     return [beverage_mock() for _ in range(5)]
+
+
+@pytest.fixture
+def create_beverage(client, beverage_uri) -> dict:
+    response = client.post(beverage_uri, json=beverage_mock())
+    return response
+
+
+@pytest.fixture
+def create_beverages(client, beverage_uri) -> list:
+    beverages = []
+    for _ in range(10):
+        new_beverage = client.post(beverage_uri, json=beverage_mock())
+        beverages.append(new_beverage.json)
+    return beverages
