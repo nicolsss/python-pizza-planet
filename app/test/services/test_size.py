22eed2e0367f8116b7cc7ef56d7d044a95f7dab5
Nicole Garcia <nicolsss>
2023-03-20 00:30:42 -0500

    test: create size service

--- app/test/services/test_size.py
+++ app/test/services/test_size.py
@@ -1,5 +1,15 @@
 import pytest
 
+
+def test_create_size_service(create_size):
+    size = create_size.json
+
+    pytest.assume(create_size.status.startswith('200'))
+    pytest.assume(size['_id'])
+    pytest.assume(size['name'])
+    pytest.assume(size['price'])
+
+ 
 def test_get_sizes_service(client, create_sizes, size_uri):
     response = client.get(size_uri)
     pytest.assume(response.status.startswith('200'))
