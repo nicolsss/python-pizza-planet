25e35a50ac47de49c4cc11ae2f7edd6413da6d29
Nicole Garcia <nicolsss>
2023-03-19 20:33:23 -0500

    add: base wrapper to size services

--- app/services/size.py
+++ app/services/size.py
@@ -1,38 +1,32 @@
 from app.common.http_methods import GET, POST, PUT
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, request
 
 from ..controllers import SizeController
+from ..services.base import base_service
+
 
 size = Blueprint('size', __name__)
 
 
 @size.route('/', methods=POST)
+@base_service
 def create_size():
-    size, error = SizeController.create(request.json)
-    response = size if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
+    return SizeController.create(request.json)
 
 
 @size.route('/', methods=PUT)
+@base_service
 def update_size():
-    size, error = SizeController.update(request.json)
-    response = size if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
+    return SizeController.update(request.json)
 
 
 @size.route('/id/<_id>', methods=GET)
+@base_service
 def get_size_by_id(_id: int):
-    size, error = SizeController.get_by_id(_id)
-    response = size if not error else {'error': error}
-    status_code = 200 if size else 404 if not error else 400
-    return jsonify(response), status_code
+    return SizeController.get_by_id(_id)
 
 
 @size.route('/', methods=GET)
+@base_service
 def get_sizes():
-    sizes, error = SizeController().get_all()
-    response = sizes if not error else {'error': error}
-    status_code = 200 if sizes else 404 if not error else 400
-    return jsonify(response), status_code
+    return SizeController().get_all()
