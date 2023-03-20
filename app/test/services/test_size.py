d2f063f7514f8f453eff756ce4710d68c1ac7737
Nicole Garcia <nicolsss>
2023-03-20 00:34:22 -0500

    test: update size service

--- app/test/services/test_size.py
+++ app/test/services/test_size.py
@@ -1,5 +1,7 @@
 import pytest
 
+from app.test.utils.functions import get_random_string, get_random_price
+
 
 def test_create_size_service(create_size):
     size = create_size.json
@@ -9,7 +11,22 @@ def test_create_size_service(create_size):
     pytest.assume(size['name'])
     pytest.assume(size['price'])
 
- 
+
+def test_update_size_service(client, create_size, size_uri):
+    # Arrange
+    current_size = create_size.json
+    update_data = {**current_size, 'name': get_random_string(), 'price': get_random_price(1, 5)}
+    
+    # Act
+    response = client.put(size_uri, json=update_data)
+    
+    # Assert
+    pytest.assume(response.status.startswith('200'))
+    updated_size = response.json
+    for param, value in update_data.items():
+        pytest.assume(updated_size[param] == value)
+
+
 def test_get_sizes_service(client, create_sizes, size_uri):
     response = client.get(size_uri)
     pytest.assume(response.status.startswith('200'))
