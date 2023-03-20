b0c4184617a5341a9ea16c42cc8dda90b121577b
nicolsss <56645701+nicolsss@users.noreply.github.com>
2023-03-20 03:03:56 -0500

    refactor: PIZ4.3 - Refactor unit tests (#10)
    * refactor: beverage controller test names
    
    * refactor: ingredient controller test names
    
    * refactor: size controller test names
    
    * refactor: order controller test names
    
    * refactor: test beverage name
    
    ---------
    
    Co-authored-by: Nicole Garcia <nicolsss>

--- app/test/controllers/test_order.py
+++ app/test/controllers/test_order.py
@@ -29,12 +29,17 @@ def __create_sizes_and_ingredients(ingredients: list, sizes: list):
     return created_sizes if len(created_sizes) > 1 else created_sizes.pop(), created_ingredients
 
 
-def test_create(app, ingredients, size, client_data):
+def test__create_order__when_functino_get_ingredients_size_and_client_data__should_return_an_order(app, ingredients, size, client_data):
+    # Arrange
     created_size, created_ingredients = __create_sizes_and_ingredients(ingredients, [size])
     order = __order(created_ingredients, created_size, client_data)
+    
+    # Act
     created_order, error = OrderController.create(order)
     size_id = order.pop('size_id', None)
     ingredient_ids = order.pop('ingredients', [])
+    
+    # Assert
     pytest.assume(error is None)
     for param, value in order.items():
         pytest.assume(param in created_order)
@@ -46,20 +51,30 @@ def test_create(app, ingredients, size, client_data):
         pytest.assume(not ingredients_in_detail.difference(ingredient_ids))
 
 
-def test_calculate_order_price(app, ingredients, size, client_data):
+def test__calculate_order_price__when_fucntion_gets_ingredients_size_and_client_data__should_return_the_total_of_all_prices(app, ingredients, size, client_data):
+    # Arrange
     created_size, created_ingredients = __create_sizes_and_ingredients(ingredients, [size])
     order = __order(created_ingredients, created_size, client_data)
+    
+    # Act
     created_order, _ = OrderController.create(order)
+    
+    # Assert
     pytest.assume(created_order['total_price'] == round(created_size['price'] + sum(ingredient['price'] for ingredient in created_ingredients), 2))
 
 
-def test_get_by_id(app, ingredients, size, client_data):
+def test__get_order_by_id__when_function_gets_ingredients_size_and_client_data__should_return_an_order(app, ingredients, size, client_data):
+    # Arrange
     created_size, created_ingredients = __create_sizes_and_ingredients(ingredients, [size])
     order = __order(created_ingredients, created_size, client_data)
     created_order, _ = OrderController.create(order)
+
+    # Act
     order_from_db, error = OrderController.get_by_id(created_order['_id'])
     size_id = order.pop('size_id', None)
     ingredient_ids = order.pop('ingredients', [])
+    
+    # Assert
     pytest.assume(error is None)
     for param, value in created_order.items():
         pytest.assume(order_from_db[param] == value)
@@ -69,7 +84,8 @@ def test_get_by_id(app, ingredients, size, client_data):
         pytest.assume(not ingredients_in_detail.difference(ingredient_ids))
 
 
-def test_get_all(app, ingredients, sizes, client_data):
+def test__get_all_orders__when_function_gets_ingredients_sizes_and_client_data__should_return_all_orders(app, ingredients, sizes, client_data):
+    # Arrange
     created_sizes, created_ingredients = __create_sizes_and_ingredients(ingredients, sizes)
     created_orders = []
     for _ in range(5):
@@ -77,8 +93,11 @@ def test_get_all(app, ingredients, sizes, client_data):
         created_order, _ = OrderController.create(order)
         created_orders.append(created_order)
 
+    # Act
     orders_from_db, error = OrderController.get_all()
     searchable_orders = {db_order['_id']: db_order for db_order in orders_from_db}
+    
+    # Assert
     pytest.assume(error is None)
     for created_order in created_orders:
         current_id = created_order['_id']
