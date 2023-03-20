f2b3f98160544eb52ea96b1df72f50fe091311af
Nicole Garcia <nicolsss>
2023-03-19 20:32:41 -0500

    add: base wrapper to beverage services

--- app/services/beverage.py
+++ app/services/beverage.py
@@ -1,37 +1,32 @@
 from app.common.http_methods import GET, POST, PUT
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, request
 
 from ..controllers import BeverageController
+from ..services.base import base_service
+
 
 beverage = Blueprint('beverage', __name__)
 
+
 @beverage.route('/', methods=POST)
+@base_service
 def create_beverage():
-    beverage, error = BeverageController().create(request.json)
-    response = beverage if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
+    return BeverageController.create(request.json)
 
 
 @beverage.route('/', methods=PUT)
+@base_service
 def update_beverage():
-    beverage, error = BeverageController.update(request.json)
-    response = beverage if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
+    return BeverageController.update(request.json)
 
 
 @beverage.route('/id/<_id>', methods=GET)
+@base_service
 def get_beverage_by_id(_id: int):
-    beverage, error = BeverageController.get_by_id(_id)
-    response = beverage if not error else {'error': error}
-    status_code = 200 if beverage else 404 if not error else 400
-    return jsonify(response), status_code
+    return BeverageController.get_by_id(_id)
 
 
 @beverage.route('/', methods=GET)
+@base_service
 def get_beverages():
-    beverages, error = BeverageController.get_all()
-    response = beverages if not error else {'error': error}
-    status_code = 200 if beverages else 404 if not error else 400
-    return jsonify(response), status_code
+    return BeverageController.get_all()
